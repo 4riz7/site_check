ebd9ae516ae9c9b3614af5a80a45314d578a90a6
4riz7_pc <vittmin47@gmail.com>
2024-09-13 15:22:16 +0300

    1

---
+++ load.py
@@ -0,0 +1,41 @@
+import requests
+
+# API ключ и ID вашей поисковой системы (cx)
+api_key = 'AIzaSyAHUc6jZwKirHD2KDBOhhpKT9dQsr8HMXQ'
+cse_id = '1189c16ca9ff3447e'
+
+# Функция для поиска
+def google_search(query, num_results=100, start=1):
+    url = 'https://www.googleapis.com/customsearch/v1'
+    params = {
+        'key': api_key,
+        'cx': cse_id,
+        'q': query,
+        'num': min(num_results, 10),  # Максимум 10 результатов за запрос
+        'start': start
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+# Пример использования
+query = 'факты'
+all_results = []
+start_index = 1
+max_results = 100  # Ограничиваем максимальное количество результатов
+
+# Открываем файл для записи
+with open('site.txt', 'w', encoding='utf-8') as file:
+    while start_index <= max_results:  # Запрашиваем только до 100 результатов
+        data = google_search(query, num_results=100, start=start_index)
+        results = data.get('items', [])
+        all_results.extend(results)
+        
+        if len(results) == 0:  # Если больше нет результатов
+            break
+        
+        start_index += 10
+
+    # Записываем результаты в файл
+    for i, result in enumerate(all_results, start=1):
+        file.write(f"{i}. {result['link']}\n")
